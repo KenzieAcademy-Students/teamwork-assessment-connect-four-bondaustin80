a3f048e75b9fc61340606bbec70bc3639c87e1b4
Austin Bond <bondaustin80@gmail.com>
2021-03-30 11:19:00 -0400

    Made skeleton of functions

--- code.js
+++ code.js
@@ -10,3 +10,35 @@ let boardLayout = [
     [null, null, null, null, null, null],
     [null, null, null, null, null, null]
 ]
+
+
+
+
+function checkHorizontal(board) {
+
+}
+
+function checkVertical(board) {
+    
+}
+
+function checkDiagonalUp(board) {
+    
+}
+
+function checkDiagonalDown(board) {
+
+}
+
+function analyzeBoard(board) {
+    if (checkHorizontal(board)) {
+        return true
+    } else if (checkVertical(board)) {
+        return true
+    } else if (checkDiagonalUp(board)) {
+        return true
+    } else if (checkDiagonalDown(board)) {
+        return true
+    }
+    return false
+}
