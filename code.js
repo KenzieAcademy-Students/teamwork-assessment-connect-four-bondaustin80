d8e8ea357e1699a0709f26e49f3dd6a49475e6a7
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-03-30 15:38:04 +0000

    deploy: 70d793b9830327287b5efd5675213dc341a5ab47

--- code.js
+++ code.js
@@ -1,44 +1,40 @@
-
 // Your Code Here.
 
 let boardLayout = [
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null]
-]
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+];
 
+function checkHorizontal(board) {}
 
+function checkVertical(board) {}
 
+function checkDiagonalUp(board) {}
 
-function checkHorizontal(board) {
-
-}
-
-function checkVertical(board) {
-    
-}
-
-function checkDiagonalUp(board) {
-    
-}
-
-function checkDiagonalDown(board) {
-
-}
+function checkDiagonalDown(board) {}
 
 function analyzeBoard(board) {
-    if (checkHorizontal(board)) {
-        return true
-    } else if (checkVertical(board)) {
-        return true
-    } else if (checkDiagonalUp(board)) {
-        return true
-    } else if (checkDiagonalDown(board)) {
-        return true
-    }
-    return false
+  if (checkHorizontal(board)) {
+    return true;
+  } else if (checkVertical(board)) {
+    return true;
+  } else if (checkDiagonalUp(board)) {
+    return true;
+  } else if (checkDiagonalDown(board)) {
+    return true;
+  }
+  return false;
 }
+
+let firstColumn = document.getElementById("column1");
+let secondColumn = document.getElementById("column2");
+let thirdColumn = document.getElementById("column3");
+let fourthColumn = document.getElementById("column4");
+let fifthColumn = document.getElementById("column5");
+let sixthColumn = document.getElementById("column6");
+let seventhColumn = document.getElementById("column7");
